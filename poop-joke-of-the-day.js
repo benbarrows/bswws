2fbf73f8fe17e50a227c25eb5b3edc00b1a7f43f
Ben Barrows <39225184+benbarrows@users.noreply.github.com>
2023-02-06 12:03:50 -0600

    Created poop-joke-of-the-day HTML and JavaScript files

---
+++ poop-joke-of-the-day.js
@@ -0,0 +1,22 @@
+async function getJoke() {
+  const response = await fetch('https://api.openai.com/v1/engines/text-davinci-002/jobs', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer <YOUR_API_KEY>'
+    },
+    body: JSON.stringify({
+      prompt: 'Generate a poop joke.',
+      max_tokens: 100,
+      n: 1,
+      stop: '\n',
+      temperature: 0.5
+    })
+  });
+  const data = await response.json();
+  const joke = data.choices[0].text;
+  document.getElementById("joke").innerHTML = joke;
+}
+
+getJoke();
+setInterval(getJoke, 24 * 60 * 60 * 1000);
